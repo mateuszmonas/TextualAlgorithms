d6d92d768d16ac30d06d7cdfd15cf1dfe5e4dd77
mateuszmonas <mateuszmonas@gmail.com>
2020-04-02 00:38:29 +0200

    asd

--- test.py
+++ test.py
@@ -1,5 +1,7 @@
 from typing import Dict
 
+slow_find_count = 0
+fast_find_count = 0
 
 class SuffixTreeNode:
 
@@ -13,13 +15,14 @@ class SuffixTreeNode:
         self.parent: SuffixTreeNode = parent
         self.link: SuffixTreeNode = None
 
-    def graft(self, start):
+    def graft(self, start) -> 'SuffixTreeNode':
         start = start + self.depth
         text = self.full_text[start:]
         child = SuffixTreeNode(self.full_text, start, len(self.full_text), self.depth + len(text), self)
         self.children[text[0]] = child
+        return child
 
-    def break_path(self, text: str):
+    def break_path(self, text: str) -> 'SuffixTreeNode':
         length = len(text)
         child = self.children[text[0]]
         new_node = SuffixTreeNode(self.full_text, child.start, child.start + length, self.depth + length, self)
@@ -29,7 +32,9 @@ class SuffixTreeNode:
         self.children[text[0]] = new_node
         return new_node
 
-    def fast_find(self, text: str):
+    def fast_find(self, text: str) -> 'SuffixTreeNode':
+        if len(text) == 0:
+            return self
         child = self.children[text[0]]
         if len(child.label) < len(text):
             return child.fast_find(text[len(child.label):])
@@ -69,18 +74,48 @@ class SuffixTreeNode:
 class SuffixTree:
 
     def __init__(self, text: str) -> None:
-        self.root: SuffixTreeNode = SuffixTreeNode(text)
-        self.root.graft(0)
+        self.root = old_head = SuffixTreeNode(text)
+        leaf = self.root.graft(0)
         for i in range(1, len(text)):
-            head= self.root.slow_find(text[i:])
-            head.graft(i)
+            if old_head == self.root:
+                old_head = self.root.slow_find(leaf.label[1:])
+                leaf = old_head.graft(i)
+                continue
+            parent = old_head.parent
+            if parent == self.root:
+                head_slink = parent.fast_find(old_head.label[1:])
+            else:
+                head_slink = parent.link.fast_find(old_head.label)
+            if len(head_slink.children) == 1:
+                new_head = head_slink
+            else:
+                new_head = head_slink.slow_find(leaf.label)
+            leaf = new_head.graft(i)
+            old_head.link = head_slink
+            old_head = new_head
 
     def __contains__(self, item):
         return isinstance(item, str) and item in self.root
 
 
-text = "ofihinweoiurrfnaiewubfiuaw;"
-tree = SuffixTree(text)
-for i in range(0, len(text)):
-    print(text[i:] in tree)
+from time import perf_counter
 
+
+def time_eval(func, args, w_print=False, name=None, count=10):
+    start = perf_counter()
+    for i in range(count):
+        func(*args)
+    end = perf_counter()
+    avg = (end-start)/count
+    if w_print:
+        print(f"{name} average time: {avg}")
+    else:
+        return avg
+
+
+with open('1997_714.txt', 'r') as file:
+    text = file.read()
+    text = text[:2000]
+    text += '\0'
+    tree = SuffixTree(text)
+    time_eval(tree.root.fast_find, [text], True, "fast", count=10)
